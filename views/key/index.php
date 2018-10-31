8c789cf1d8012868d189ccdd58098bf12e2f3e64
zabrosoft <zaikinnv@mail.ru>
2018-10-31 16:15:52 +0500

    Поиск по ключу. Проверка.

--- views/key/index.php
+++ views/key/index.php
@@ -4,7 +4,15 @@
 <div class="row">
     <div class="col-md-3"></div>
     <div class="col-md-6">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Введите номер брелка" id="txt_number_key">
+            <span class="input-group-btn">
+                <button class="btn btn-default" type="button" onclick="searchByNumberKey()">Найти</button>
+            </span>
+        </div><!-- /input-группа -->
+        <div id="result">
 
+        </div>
     </div>
     <div class="col-md-3"></div>
 </div>
---
+++ web/js/script.js
@@ -0,0 +1,3 @@
+function searchByNumberKey(){
+    alert('Ищу');
+}
